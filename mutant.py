97cea309791d11a48f6a118ad4fdc09150a6462a
Dmitry Dementev <dem@rubear.biz>
2013-03-05 17:00:28 +0400

    language running script

--- mutant.py
+++ mutant.py
@@ -1,2 +1,35 @@
 #!/usr/bin/env python
 import sys
+import os
+import os.path
+
+MUTANT_FILE = os.path.realpath(__file__)
+MUTANT_PATH = os.path.dirname(MUTANT_FILE)
+CURRENT_PATH = os.path.abspath('.')
+
+
+HELP = """
+usage:
+  mutant js
+
+  mutant py
+"""
+
+def usage():
+  print HELP
+
+def taskJs():
+  print 'task js not implemented'
+
+def taskPy():
+  print 'task py not implemented'
+
+def main():
+  taskMap = {
+      'js': taskJs,
+      'py': taskPy,
+    }
+  print 'main'
+
+if __name__ == '__main__':
+  sys.exit(main())
